fc7c981193ac4d11851055d195ce43f72a4f2c59
kirillsukhorukov <sukhorukov_kirill@mail.ru>
2023-03-15 12:25:41 +0700

    initial

---
+++ 16.6-Task-2-Fractional-stitcher.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+double stitcher (const string& integer, const string& fractional)
+{
+    string tempStr = integer + '.' + fractional;
+    cout << tempStr << endl;
+    return stod(tempStr);
+}
+
+int main()
+{
+    cout << "-----Fractional stitcher-----" << endl;
+    //Инициализация переменной точности отображения числа при выводе
+    int precision = 10;
+    //Пользовательский ввод целой и дробной части чмсел
+    string integer;    
+    cout << "Enter the integer part of the number: ";
+    cin >> integer;
+    string fractional;
+    cout << "Enter the fractional part of the number: ";
+    cin >> fractional;
+    //Вывод результата с заданной точностью
+    cout << setprecision(precision) << "Result: "<< stitcher(integer, fractional) << endl;
+    return 0;
+}
